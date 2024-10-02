70663e50d696906a67115e47b81e403cf4f0651d
RexshackGaming <rexshackuk@gmail.com>
2024-10-02 15:30:11 +0100

    Update weapons.lua

--- shared/weapons.lua
+++ shared/weapons.lua
@@ -1,13 +1,41 @@
 RSGShared = RSGShared or {}
 RSGShared.Weapons = {
     -- revolver
-	[`weapon_revolver_cattleman`]            = { name = 'weapon_revolver_cattleman',            label = 'Cattleman',             weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_cattleman_mexican`]    = { name = 'weapon_revolver_cattleman_mexican',    label = 'Cattleman Mexican',     weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_doubleaction`]         = { name = 'weapon_revolver_doubleaction',         label = 'Double Action',         weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_doubleaction_gambler`] = { name = 'weapon_revolver_doubleaction_gambler', label = 'Double Action Gambler', weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_schofield`]            = { name = 'weapon_revolver_schofield',            label = 'Schofield',             weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_lemat`]                = { name = 'weapon_revolver_lemat',                label = 'Lemat',                 weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_navy`]                 = { name = 'weapon_revolver_navy',                 label = 'Navy',                  weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-	[`weapon_revolver_navy_crossover`]       = { name = 'weapon_revolver_navy_crossover',       label = 'Navy Crossover',        weapontype = 'Revolver', ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
-
+	[`weapon_revolver_cattleman`]              = { name = 'weapon_revolver_cattleman',              label = 'Cattleman Revolver',                 weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_cattleman_mexican`]      = { name = 'weapon_revolver_cattleman_mexican',      label = 'Cattleman Mexican Revolver',         weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_doubleaction`]           = { name = 'weapon_revolver_doubleaction',           label = 'Double Action Revolver',             weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_doubleaction_gambler`]   = { name = 'weapon_revolver_doubleaction_gambler',   label = 'Double Action Gambler Revolver',     weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_schofield`]              = { name = 'weapon_revolver_schofield',              label = 'Schofield Revolver',                 weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_lemat`]                  = { name = 'weapon_revolver_lemat',                  label = 'Lemat Revolver',                     weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_navy`]                   = { name = 'weapon_revolver_navy',                   label = 'Navy Revolver',                      weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+	[`weapon_revolver_navy_crossover`]         = { name = 'weapon_revolver_navy_crossover',         label = 'Navy Crossover Revolver',            weapontype = 'Revolver',     ammotype = 'AMMO_REVOLVER', damagereason = 'Placeholder' },
+    -- pistol
+	[`weapon_pistol_volcanic`]                 = { name = 'weapon_pistol_volcanic',                 label = 'Volcanic Pistol',                    weapontype = 'Pistol',       ammotype = 'AMMO_PISTOL',   damagereason = 'Placeholder' },
+	[`weapon_pistol_m1899`]                    = { name = 'weapon_pistol_m1899',                    label = 'm1899 Pistol',                       weapontype = 'Pistol',       ammotype = 'AMMO_PISTOL',   damagereason = 'Placeholder' },
+	[`weapon_pistol_mauser`]                   = { name = 'weapon_pistol_mauser',                   label = 'Mauser Pistol',                      weapontype = 'Pistol',       ammotype = 'AMMO_PISTOL',   damagereason = 'Placeholder' },
+	[`weapon_pistol_semiauto`]                 = { name = 'weapon_pistol_semiauto',                 label = 'Semi-Auto Pistol',                   weapontype = 'Pistol',       ammotype = 'AMMO_PISTOL',   damagereason = 'Placeholder' },
+    -- repeater
+	[`weapon_repeater_carbine`]                = { name = 'weapon_repeater_carbine',                label = 'Carbine Repeater',                   weapontype = 'Repeater',     ammotype = 'AMMO_REPEATER', damagereason = 'Placeholder' },
+	[`weapon_repeater_winchester`]             = { name = 'weapon_repeater_winchester',             label = 'Winchester Repeater',                weapontype = 'Repeater',     ammotype = 'AMMO_REPEATER', damagereason = 'Placeholder' },
+	[`weapon_repeater_henry`]                  = { name = 'weapon_repeater_henry',                  label = 'Henry Repeater',                     weapontype = 'Repeater',     ammotype = 'AMMO_REPEATER', damagereason = 'Placeholder' },
+	[`weapon_repeater_evans`]                  = { name = 'weapon_repeater_evans',                  label = 'Evans Repeater',                     weapontype = 'Repeater',     ammotype = 'AMMO_REPEATER', damagereason = 'Placeholder' },
+    -- rifle
+	[`weapon_rifle_varmint`]                   = { name = 'weapon_rifle_varmint',                   label = 'Varmint Rifle',                      weapontype = 'Rifle',        ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+	[`weapon_rifle_springfield`]               = { name = 'weapon_rifle_springfield',               label = 'Springfield Rifle',                  weapontype = 'Rifle',        ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+	[`weapon_rifle_boltaction`]                = { name = 'weapon_rifle_boltaction',                label = 'Boltaction Rifle',                   weapontype = 'Rifle',        ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+	[`weapon_rifle_elephant`]                  = { name = 'weapon_rifle_elephant',                  label = 'Elephant Rifle',                     weapontype = 'Rifle',        ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+    -- sniperrifle
+	[`weapon_sniperrifle_rollingblock`]        = { name = 'weapon_sniperrifle_rollingblock',        label = 'Rollingblock Sniper Rifle',          weapontype = 'Sniper Rifle', ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+	[`weapon_sniperrifle_rollingblock_exotic`] = { name = 'weapon_sniperrifle_rollingblock_exotic', label = 'Rollingblock Exotic Sniper Rifle',   weapontype = 'Sniper Rifle', ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+	[`weapon_sniperrifle_carcano`]             = { name = 'weapon_sniperrifle_carcano',             label = 'Rollingblock Carcano Sniper Rifle',  weapontype = 'Sniper Rifle', ammotype = 'AMMO_RIFLE',    damagereason = 'Placeholder' },
+    -- shotgun
+	[`weapon_shotgun_doublebarrel`]            = { name = 'weapon_shotgun_doublebarrel',            label = 'Double Barrel Shotgum',              weapontype = 'Shotgun',      ammotype = 'AMMO_SHOTGUN',  damagereason = 'Placeholder' },
+	[`weapon_shotgun_doublebarrel_exotic`]     = { name = 'weapon_shotgun_doublebarrel_exotic',     label = 'Double Barrel Exotic Shotgum',       weapontype = 'Shotgun',      ammotype = 'AMMO_SHOTGUN',  damagereason = 'Placeholder' },
+	[`weapon_shotgun_sawedoff`]                = { name = 'weapon_shotgun_sawedoff',                label = 'Sawedoff Shotgum',                   weapontype = 'Shotgun',      ammotype = 'AMMO_SHOTGUN',  damagereason = 'Placeholder' },
+	[`weapon_shotgun_semiauto`]                = { name = 'weapon_shotgun_semiauto',                label = 'Semi-Auto Shotgum',                  weapontype = 'Shotgun',      ammotype = 'AMMO_SHOTGUN',  damagereason = 'Placeholder' },
+	[`weapon_shotgun_pump`]                    = { name = 'weapon_shotgun_pump',                    label = 'Pump Action Shotgun',                weapontype = 'Shotgun',      ammotype = 'AMMO_SHOTGUN',  damagereason = 'Placeholder' },
+	[`weapon_shotgun_repeating`]               = { name = 'weapon_shotgun_repeating',               label = 'Repeating Shotgun',                  weapontype = 'Shotgun',      ammotype = 'AMMO_SHOTGUN',  damagereason = 'Placeholder' },
+    -- bow
+	[`weapon_bow`]                             = { name = 'weapon_bow',                             label = 'Sandard Bow',                        weapontype = 'Bow',          ammotype = 'AMMO_ARROW',    damagereason = 'Placeholder' },
+	[`weapon_bow_improved`]                    = { name = 'weapon_bow',                             label = 'Improved Bow',                       weapontype = 'Bow',          ammotype = 'AMMO_ARROW',    damagereason = 'Placeholder' },
 }
